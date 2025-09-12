f1ac9357f5ed18132d3bd159046ce89b893ab70d
lu-antony <lu-antony@yahoo.com.tw>
2025-09-12 16:35:04 +0800

    add DisplayName, Photo for ApplicationUser

--- ApiServer.WebApi/BusinessObjects/ApplicationUser.cs
+++ ApiServer.WebApi/BusinessObjects/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl.PermissionPolicy;
 using DevExpress.Xpo;
 using System.ComponentModel;
@@ -6,7 +7,8 @@ using System.ComponentModel;
 namespace ApiServer.WebApi.BusinessObjects;
 
 [MapInheritance(MapInheritanceType.ParentTable)]
-[DefaultProperty(nameof(UserName))]
+[DefaultProperty(nameof(DisplayName))]
+[CurrentUserDisplayImage(nameof(Photo))]
 public class ApplicationUser : PermissionPolicyUser, ISecurityUserWithLoginInfo, ISecurityUserLockout
 {
     private int accessFailedCount;
@@ -45,4 +47,45 @@ public class ApplicationUser : PermissionPolicyUser, ISecurityUserWithLoginInfo,
         result.User = this;
         return result;
     }
+
+    #region DefaultProperty => DisplayName
+
+    private string _DisplayName;
+
+    [Description("The DisplayName property represents the full name of the user. It is used as the default display name in the application.")]
+    public string DisplayName
+    {
+        get { return _DisplayName; }
+        set { SetPropertyValue<string>(nameof(DisplayName), ref _DisplayName, value); }
+    }
+
+    private string _Email;
+
+    [Description("The Email property stores the email address of the user. It is used for communication and authentication purposes.")]
+    public string Email
+    {
+        get { return _Email; }
+        set { SetPropertyValue<string>(nameof(Email), ref _Email, value); }
+    }
+
+    [Description("The Photo property stores the user's profile picture as a byte array. It is displayed in the user interface to represent the user visually.")]
+    [ImageEditor(ListViewImageEditorMode = ImageEditorMode.PictureEdit,
+    DetailViewImageEditorMode = ImageEditorMode.PictureEdit)]
+    public byte[] Photo
+    {
+        get { return GetPropertyValue<byte[]>(nameof(Photo)); }
+        set
+        {
+            SetPropertyValue<byte[]>(nameof(Photo), value);
+        }
+    }
+
+    protected override void OnSaving()
+    {
+        base.OnSaving();
+        if (string.IsNullOrEmpty(DisplayName))
+            DisplayName = UserName;
+    }
+
+    #endregion
 }
